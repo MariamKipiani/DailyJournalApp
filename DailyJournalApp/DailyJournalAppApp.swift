d658251adb7449c2e34de3a25ba1cfa7a03b6c3a
MariamKipiani <145837678+MariamKipiani@users.noreply.github.com>
2023-12-20 23:39:18 +0400

    Initial Commit

---
+++ DailyJournalApp/DailyJournalAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyJournalAppApp.swift
+//  DailyJournalApp
+//
+//  Created by user on 12/20/23.
+//
+
+import SwiftUI
+
+@main
+struct DailyJournalAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
